874fc7d60aba77b5528204344813cbd6fdf02807
guobj <18253695103@163.com>
2018-05-13 14:30:08 +0800

    数据库

--- src/main/resources/newsapp.sql
+++ src/main/resources/newsapp.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50520
 File Encoding         : 65001
 
-Date: 2018-04-30 21:47:57
+Date: 2018-05-13 14:29:17
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -33,6 +33,24 @@ INSERT INTO `authority` VALUES ('2', '管理员');
 INSERT INTO `authority` VALUES ('3', '普通员工');
 
 -- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `c_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_n_id` int(11) DEFAULT NULL,
+  `fk_u_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `comment` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES ('10', '39', '33', '2018-05-12 16:41:21', '测试评论-1');
+
+-- ----------------------------
 -- Table structure for employee
 -- ----------------------------
 DROP TABLE IF EXISTS `employee`;
@@ -50,17 +68,14 @@ CREATE TABLE `employee` (
   `e_dr` tinyint(1) DEFAULT NULL,
   `e_creat_time` datetime DEFAULT NULL,
   PRIMARY KEY (`e_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of employee
 -- ----------------------------
-INSERT INTO `employee` VALUES ('1', '超级管理员', '0', '24', '济南', null, '1234567889', '12345677@1233.com', 'admin', '1234', '1', null);
-INSERT INTO `employee` VALUES ('2', 'wewe', '1', '33', '454', '3', '123', '45', '234', '1234', '1', null);
-INSERT INTO `employee` VALUES ('3', '阴艳芹', '1', '34', '烟台', '3', '2354545645', '344@123.com', 'nhk', '1234', '1', null);
-INSERT INTO `employee` VALUES ('4', '最忠实的粉丝', '1', '445', '32342', '3', '3435345', '2342', '323', '1234', '0', null);
-INSERT INTO `employee` VALUES ('5', '黄金时代不回家', '0', '34', '234', '3', '234234234', '34', '34', '1234', '1', null);
-INSERT INTO `employee` VALUES ('6', '现在', '0', '34', '324', '3', '32342', '2342', '1123', '1234', '1', null);
+INSERT INTO `employee` VALUES ('1', '超级管理员', '1', '24', '济南', null, '1234567889', '12345677@1233.com', 'admin', '1234', '1', null);
+INSERT INTO `employee` VALUES ('7', '阴艳芹', '1', '24', '济南', '3', '17812345678', '14@163.con', 'yinyq', '1234', '1', '2018-05-12 14:25:49');
+INSERT INTO `employee` VALUES ('8', '阴艳芹-2', '1', '25', '济南', '2', '17812345678', '12@163.com', 'yinyq2', '1234', '0', '2018-05-12 14:28:12');
 
 -- ----------------------------
 -- Table structure for news
@@ -69,33 +84,27 @@ DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news` (
   `n_id` int(11) NOT NULL AUTO_INCREMENT,
   `content` varchar(5000) DEFAULT NULL,
-  `auth` varchar(255) DEFAULT NULL,
   `img` varchar(255) DEFAULT NULL,
   `fk_nt_id` varchar(255) DEFAULT NULL,
   `creat_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
+  `auth` int(255) DEFAULT NULL,
+  `sign` int(11) DEFAULT NULL COMMENT '公司级用户级，0公司，1用户',
   `dr` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`n_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of news
 -- ----------------------------
-INSERT INTO `news` VALUES ('1', '我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！', '阴艳芹', 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', null, '2018-04-29 21:40:02', '阴艳芹是坏蛋', '1');
-INSERT INTO `news` VALUES ('2', '我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！', '郭宾杰', 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', null, null, '阴艳芹是坏蛋', '1');
-INSERT INTO `news` VALUES ('3', '郭宾杰', null, 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', null, null, '阴艳芹是坏蛋', '1');
-INSERT INTO `news` VALUES ('4', null, null, 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', null, null, '阴艳芹是坏蛋', '1');
-INSERT INTO `news` VALUES ('5', null, null, 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', null, null, '阴艳芹是坏蛋', '1');
-INSERT INTO `news` VALUES ('6', null, null, 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', null, null, '阴艳芹是坏蛋', '1');
-INSERT INTO `news` VALUES ('7', null, '234', 'a82d58c2b10b46e3a7b971406d20f568.PNG', '34', null, null, '234324', '1');
-INSERT INTO `news` VALUES ('8', null, '二', 'a82d58c2b10b46e3a7b971406d20f568.PNG', 'e', null, null, '额', '1');
-INSERT INTO `news` VALUES ('9', null, '二', 'a82d58c2b10b46e3a7b971406d20f568.PNG', 'e', null, null, '额', '1');
-INSERT INTO `news` VALUES ('10', null, '问', 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', null, null, '我', '1');
-INSERT INTO `news` VALUES ('11', null, '我', 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', '2018-04-19 20:54:16', '2018-04-19 20:54:16', 'u', '0');
-INSERT INTO `news` VALUES ('12', '我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！我对阴艳芹那么好！！她居然骗我，她是个坏蛋，不想要它了。她是骗子，还不让说，脾气那么差！', '343', 'a82d58c2b10b46e3a7b971406d20f568.PNG', '1', '2018-04-26 15:37:11', '2018-04-26 15:37:11', '343434', '1');
-INSERT INTO `news` VALUES ('13', '飒飒大苏打的', '232', '2cf6dd16279345e299129e2004966c8e.jpg', '1', '2018-04-26 15:44:00', '2018-04-26 15:44:00', '2323', '1');
-INSERT INTO `news` VALUES ('14', '<p>微软微软</p>', '为', '3428d38461c244cb815c7590348c9dec.PNG', '1', '2018-04-26 15:58:07', '2018-04-26 15:58:07', '微软', '1');
+INSERT INTO `news` VALUES ('33', '<p>测试-1</p>', '7d41c9bc98ff42dfa94113aeaf6ad1d0.png', '3', '2018-05-12 14:17:34', '2018-05-12 14:18:05', '测试-1-update', '1', '0', '1');
+INSERT INTO `news` VALUES ('34', '<p>测试-2</p>', 'b1fdae6a77f14729b692c54e2cc99576.png', '1', '2018-05-12 14:18:47', '2018-05-12 14:18:47', '测试-2', '1', '0', '1');
+INSERT INTO `news` VALUES ('35', '<p>测试-3</p>', 'c330a491610049abbe4666daee6871a1.png', '2', '2018-05-12 14:19:16', '2018-05-12 14:19:16', '测试-3', '1', '0', '1');
+INSERT INTO `news` VALUES ('36', '<p>测试-4</p>', '3c48117b68d74cf4b68826d9f83cce96.png', '4', '2018-05-12 14:19:42', '2018-05-12 14:19:42', '测试-4', '1', '0', '1');
+INSERT INTO `news` VALUES ('37', '<p>测试-5</p>', '5b226ecbe1ba4e58a6f936da3b6a3998.png', '5', '2018-05-12 14:20:10', '2018-05-12 14:20:10', '测试-5', '1', '0', '1');
+INSERT INTO `news` VALUES ('38', '<p>测试-6-delete</p>', 'f348bb48475d4226a4d9add73a750302.png', '1', '2018-05-12 14:23:56', '2018-05-12 14:23:56', '测试-6-delete', '1', '0', '0');
+INSERT INTO `news` VALUES ('39', '前端测试-1', '92ea235790754644955802519092de81.png', '1', '2018-05-12 15:47:47', '2018-05-12 15:47:48', '前端测试-1', '33', '1', '1');
 
 -- ----------------------------
 -- Table structure for news_type
@@ -116,15 +125,6 @@ INSERT INTO `news_type` VALUES ('2', '政治', '1');
 INSERT INTO `news_type` VALUES ('3', '经济', '1');
 INSERT INTO `news_type` VALUES ('4', '军事', '1');
 INSERT INTO `news_type` VALUES ('5', '娱乐', '1');
-INSERT INTO `news_type` VALUES ('6', null, '0');
-INSERT INTO `news_type` VALUES ('7', null, '0');
-INSERT INTO `news_type` VALUES ('8', null, '0');
-INSERT INTO `news_type` VALUES ('9', null, '0');
-INSERT INTO `news_type` VALUES ('10', null, '0');
-INSERT INTO `news_type` VALUES ('11', null, '0');
-INSERT INTO `news_type` VALUES ('12', null, '0');
-INSERT INTO `news_type` VALUES ('13', null, '0');
-INSERT INTO `news_type` VALUES ('14', null, '0');
 
 -- ----------------------------
 -- Table structure for user
@@ -142,9 +142,11 @@ CREATE TABLE `user` (
   `u_email` varchar(255) DEFAULT NULL,
   `u_dr` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`u_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '阴艳芹', '0', '25', 'forward', '1234', '我去', 'y6y8', '324', '1');
+INSERT INTO `user` VALUES ('33', '傻子1', null, null, 'yyq1', '1234', null, null, null, '1');
+INSERT INTO `user` VALUES ('34', '傻子2', null, null, 'yyq2', '1234', null, null, null, '1');
+INSERT INTO `user` VALUES ('35', '傻子3', null, null, 'yy', '1234', null, null, null, '1');
